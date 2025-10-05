af754f68d40c4904881f46f29c16920fa125fd26
Aleksei Shestov <shestov.msu@mail.ru>
2025-10-05 20:28:30 +0300

    add_losses_backbones_dowstreams_script

--- gender_script.py
+++ gender_script.py
@@ -1,22 +1,21 @@
+from run_exp import create_params_grid, run_grid_search
 import sys
 import os
-
-sys.path.append("../google-research/graph_embedding/metrics")
-
-checkpoints_path = "gender/checkpoints_large_30"
-os.makedirs(checkpoints_path, exist_ok=True)
-os.makedirs('logs/gender', exist_ok=True)
-
-import run_models
-import run_metrics
-from run_exp import create_params_grid, run_grid_search
 import logging
 import pandas as pd
 import numpy as np
-from datetime import timedelta
+import datetime
 from ptls.preprocessing import PandasDataPreprocessor
 import torch
 from sklearn.model_selection import train_test_split
+
+now = f"{datetime.datetime.now()} BarlowTwinsLoss default"
+sys.path.append("../google-research/graph_embedding/metrics")
+checkpoints_path = f"gender/checkpoints_{now}"
+os.makedirs(checkpoints_path, exist_ok=True)
+os.makedirs(f'logs/gender_{now}', exist_ok=True)
+
+
 np.random.seed(42)
 
 
@@ -24,7 +23,8 @@ def tr_datetime_preprocess(tr_datetime):
     days, hms = tr_datetime.split()
     hh, mm, ss = hms.split(":")
 
-    seconds = timedelta(hours=int(hh), minutes=int(mm), seconds=int(ss))
+    seconds = datetime.timedelta(
+        hours=int(hh), minutes=int(mm), seconds=int(ss))
     seconds = seconds.total_seconds()
     seconds += int(days) * 24 * 3600
 
@@ -32,20 +32,20 @@ def tr_datetime_preprocess(tr_datetime):
 
 
 transactions = pd.read_csv(
-    "https://huggingface.co/datasets/dllllb/transactions-gender/resolve/main/transactions.csv.gz?download=true", compression="gzip")
+    "https://huggingface.co/datasets/dllllb/transactions-gender/resolve/main/transactions.csv.gz?download=true",
+    compression="gzip"
+)
 targets = pd.read_csv(
-    "https://huggingface.co/datasets/dllllb/transactions-gender/resolve/main/gender_train.csv?download=true")
+    "https://huggingface.co/datasets/dllllb/transactions-gender/resolve/main/gender_train.csv?download=true"
+)
+
 transactions = transactions.dropna().reset_index(drop=True)
 
 n_cutomers = len(pd.unique(transactions["customer_id"]))
 n_labeling_cutomers = len(pd.unique(targets["customer_id"]))
+
 sourceA = transactions[["customer_id", "tr_datetime", "mcc_code", "term_id"]]
 sourceB = transactions[["customer_id", "tr_datetime", "tr_type", "amount"]]
-sourceA_drop_indices = np.random.choice(sourceA.index, 130000, replace=False)
-sourceB_drop_indices = np.random.choice(sourceB.index, 420000, replace=False)
-
-sourceA = sourceA.drop(sourceA_drop_indices).reset_index(drop=True)
-sourceB = sourceB.drop(sourceB_drop_indices).reset_index(drop=True)
 
 mcc_code_in = len(np.unique((sourceA["mcc_code"])))
 term_id_in = len(np.unique((sourceA["term_id"])))
@@ -86,27 +86,48 @@ processed_sourceB.columns = [
     "sourceB_" + str(col) if str(col) != "customer_id" else str(col)
     for col in processed_sourceB.columns
 ]
+
 joined_data = processed_sourceA.merge(
-    processed_sourceB, how="outer", on="customer_id")
+    processed_sourceB,
+    how="outer",
+    on="customer_id"
+)
+
 joined_data = joined_data.applymap(
-    lambda x: torch.tensor([]) if pd.isna(x) else x)
-train_df, test_df = train_test_split(joined_data,
-                                     test_size=0.1,
-                                     random_state=42)
-train_df, valid_df = train_test_split(train_df,
-                                      test_size=0.1,
-                                      random_state=42)
-print(train_df.index.intersection(test_df.index))
-print(train_df['customer_id'].unique().shape,
-      test_df['customer_id'].unique().shape)
-print(np.unique(test_df.index.values).shape, test_df.shape)
+    lambda x: torch.tensor([]) if pd.isna(x) else x
+)
+
+train_df, test_df = train_test_split(
+    joined_data,
+    test_size=0.1,
+    random_state=42
+)
+train_df, valid_df = train_test_split(
+    train_df,
+    test_size=0.1,
+    random_state=42
+)
+
+print(
+    train_df.index.intersection(test_df.index)
+)
+print(
+    train_df['customer_id'].unique().shape,
+    test_df['customer_id'].unique().shape
+)
+print(
+    np.unique(test_df.index.values).shape,
+    test_df.shape
+)
 print(test_df.index)
+
 train_df = train_df.reset_index(drop=True)
 valid_df = valid_df.reset_index(drop=True)
 test_df = test_df.reset_index(drop=True)
 train_dict = train_df.to_dict("records")
 valid_dict = valid_df.to_dict("records")
 test_dict = test_df.to_dict("records")
+
 source_features = {
     "sourceA": ["event_time", "mcc_code", "term_id"],
     "sourceB": ["event_time", "tr_type", "amount"]
@@ -116,7 +137,8 @@ source_features = {
 logger = logging.getLogger("my_logger")
 logger.setLevel(logging.INFO)
 
-file_handler = logging.FileHandler("logs/gender/fraction_experiment.log")
+file_handler = logging.FileHandler(
+    f"logs/gender_{now}/fraction_experiment.log")
 formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
 file_handler.setFormatter(formatter)
 
@@ -126,6 +148,7 @@ if logger.hasHandlers():
 
 logger.addHandler(file_handler)
 logger.info("🔧 Логгер настроен вручную")
+
 fixed_params = {
     "batch_size": 64,
     "learning_rate": 0.001,
@@ -138,35 +161,49 @@ fixed_params = {
     "mcc_code_in": mcc_code_in,
     "term_id_in": term_id_in,
     "tr_type_in": tr_type_in,
-    "num_epochs": 30,
+    "num_epochs": 1,
     'source_features': source_features,
-    "loss": "ContrastiveLoss",
+    "loss": "BarlowTwinsLoss",
     "rnn_encoder_type": "gru"
 }
 
 # Список гиперпараметров для перебора
 variable_params = {
-    "batch_size": [16, 32, 64, 128, 256],
-    "learning_rate": [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05],
-    "split_count": [3, 5, 7],
-    "cnt_min": [5, 10, 15, 20],
-    "cnt_max": [60, 80, 100, 150, 200],
-    "embedding_dim": [32, 64, 128, 256, 512, 1024],
-    "category_embedding_dim": [4, 8, 16, 24, 32, 64, 128],
-    "hidden_size": [64, 128, 256, 512, 1024, 2048, 4096],
-    "loss": ["BarlowTwinsLoss", "ContrastiveLoss", "VicregLoss", "SoftmaxLoss"],
-    "rnn_encoder_type": ["gru", "lstm"]
+    "batch_size": [16, 32, 64, 128, 256][:1],
+    # "learning_rate": [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05][:1],
+    # "split_count": [3, 5, 7][:1],
+    # "cnt_min": [5, 10, 15, 20],
+    # "cnt_max": [60, 80, 100, 150, 200][:1],
+    # "embedding_dim": [32, 64, 128, 256, 512, 1024][:1],
+    # "category_embedding_dim": [4, 8, 16, 24, 32, 64, 128][:1],
+    # "hidden_size": [64, 128, 256, 512, 1024, 2048, 4096][:1],
+    # "loss": ["BarlowTwinsLoss", "ContrastiveLoss", "VicregLoss", "SoftmaxLoss"][:1],
+    # "rnn_encoder_type": ["gru", "lstm"][:1]
 }
-
 
 all_hyperparameter_grids = create_params_grid(fixed_params, variable_params)
 
-out_prefix = r"csv_results/gender_sample_fraction_large_30"
+
+out_folder = f"/home/dpetrovitch/dzagcoffee/output_{now}"
+out_prefix = out_folder + f"/out"
+
+os.makedirs(out_folder, exist_ok=True)
 
 sample_fractions = np.linspace(1/20, 1, 5)
 
-run_grid_search(all_hyperparameter_grids, sample_fractions,
-                train_dict, valid_dict, test_dict, targets,
-                checkpoints_path, logger, col_id="customer_id",
-                target_col='gender', out_prefix="/home/dpetrovitch/dzagcoffee/output/out", verbose=0,
-                n_samples=1)
+run_grid_search(
+    all_hyperparameter_grids,
+    sample_fractions,
+    train_dict,
+    valid_dict,
+    test_dict,
+    targets,
+    checkpoints_path,
+    logger,
+    col_id="customer_id",
+    target_col='gender',
+    out_prefix=out_prefix,
+    verbose=0,
+    n_samples=1,
+    downstream_type="logreg"  # 'catboost' | 'mlp' | 'logreg'
+)
--- run_exp.py
+++ run_exp.py
@@ -8,6 +8,7 @@ import gc
 import os
 import torch
 
+
 def clear_checkpoints_dir(checkpoints_path):
     if not os.path.exists(checkpoints_path):
         return
@@ -29,57 +30,64 @@ def create_params_grid(fixed_params, variable_params):
     for variable_param_name, variable_param_values in variable_params.items():
         for value in variable_param_values:
             hyperparameter_grid = {**fixed_params, variable_param_name: value}
-            all_hyperparameter_grids.append((variable_param_name, hyperparameter_grid))
+            all_hyperparameter_grids.append(
+                (variable_param_name, hyperparameter_grid))
 
     return all_hyperparameter_grids
 
-def run_grid_search(all_hyperparameter_grids, sample_fractions,
-        train_data_in, valid_data_in, test_data_in, targets,
-        checkpoints_path, logger,col_id="customer_id", 
-        target_col='gender', out_prefix=None,verbose=0, n_samples=10):
-    cur_time = time()
-    all_embs = []    
 
-    for param in tqdm(all_hyperparameter_grids):            
+def run_grid_search(all_hyperparameter_grids, sample_fractions,
+                    train_data_in, valid_data_in, test_data_in, targets,
+                    checkpoints_path, logger, col_id="customer_id",
+                    target_col='gender', out_prefix=None, verbose=0,
+                    n_samples=10, downstream_type="catboost"):
+    cur_time = time()
+    all_embs = []
+
+    for param in tqdm(all_hyperparameter_grids):
         logger.info(f'All params are frozen except {param[0]}')
         params = param[1]
-        model_keeper = ModelKeeper()    
+        model_keeper = ModelKeeper()
         torch.cuda.empty_cache()
         gc.collect()
-        logger.info(f"Testing parameters: {params}")        
-        model_keeper.create_datasets(train_data_in, valid_data_in, params, 
-                            col_id=col_id)
+        logger.info(f"Testing parameters: {params}")
+        model_keeper.create_datasets(train_data_in, valid_data_in, params,
+                                     col_id=col_id)
         model_keeper.train_model(params, checkpoints_path=checkpoints_path)
-       
+
         embs = model_keeper.calc_embs_from_trained(test_data_in)
-        
+
         clear_checkpoints_dir(checkpoints_path=checkpoints_path)
-        
+
         all_embs += embs
 
-    eval_many_embs(all_embs, targets, 
-        col_id=col_id, target_col=target_col, out_prefix=out_prefix, 
-        sample_fractions=sample_fractions, verbose=verbose, n_samples=n_samples)
+    eval_many_embs(all_embs, targets,
+                   col_id=col_id, target_col=target_col, out_prefix=out_prefix,
+                   sample_fractions=sample_fractions, verbose=verbose,
+                   n_samples=n_samples, downstream_type=downstream_type)
 
-def eval_many_embs(embs_list, targets, col_id='customer_id', 
-        target_col='gender', out_prefix=None, 
-        sample_fractions=tuple((1/20,)), verbose=0, n_samples=10): 
-    res_per_sample_frac = defaultdict(list) 
 
-    for curr_emb in tqdm(embs_list): 
-        res = evaluate_one_emb(curr_emb['emb'], targets, 
-            sample_fractions=sample_fractions,
-            col_id=col_id, target_col=target_col, 
-            verbose=verbose, n_samples=n_samples)
-    
+def eval_many_embs(embs_list, targets, col_id='customer_id',
+                   target_col='gender', out_prefix=None,
+                   sample_fractions=tuple((1/20,)), verbose=0,
+                   n_samples=10, downstream_type="catboost"):
+    res_per_sample_frac = defaultdict(list)
+
+    for curr_emb in tqdm(embs_list):
+        res = evaluate_one_emb(curr_emb['emb'], targets,
+                               sample_fractions=sample_fractions,
+                               col_id=col_id, target_col=target_col,
+                               verbose=verbose, n_samples=n_samples,
+                               downstream_type=downstream_type)
+
         for i, metrics in enumerate(res):
             metrics_flattened = {k: round(v, 4) for k, v in metrics.items()}
-            #times_flattened = {f"time_{k}": round(v, 4) for k, v in times.items()}
+            # times_flattened = {f"time_{k}": round(v, 4) for k, v in times.items()}
             # Сохранение результатов
             res_dict = {
                 **curr_emb['info'],
                 **metrics_flattened
-                #**times_flattened,
+                # **times_flattened,
             }
 
             res_per_sample_frac[metrics['sample_fraction']].append(res_dict)
@@ -90,9 +98,9 @@ def eval_many_embs(embs_list, targets, col_id='customer_id',
         print(new_result)
         new_result = pd.DataFrame(new_result)
 
-        # if not os.path.exists(output_csv):  
+        # if not os.path.exists(output_csv):
         #     pd.DataFrame(columns=columns).to_csv(output_csv, mode="w", index=False, header=True)
-        output_csv = f"{out_prefix}_{sample_frac:.3f}".rstrip('0').rstrip('.') + ".csv"
+        output_csv = f"{out_prefix}_{sample_frac:.3f}".rstrip(
+            '0').rstrip('.') + ".csv"
 
-        new_result.to_csv(output_csv, mode="w")#, header=False, index=False)
-
+        new_result.to_csv(output_csv, mode="w")  # , header=False, index=False)
